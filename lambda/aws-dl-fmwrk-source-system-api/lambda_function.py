f4e3821f7ec457fecdb0dbe9845b43f7f5ec4bbe
Sagar Das <sagar.das@tigeranalytics.com>
2022-05-19 14:59:49 +0530

    Adding updated lambda and utils code for source system API

--- lambda/aws-dl-fmwrk-source-system-api/lambda_function.py
+++ lambda/aws-dl-fmwrk-source-system-api/lambda_function.py
@@ -1,5 +1,4 @@
 import json
-from datetime import datetime, timezone, timedelta
 
 from connector import Connector
 from utils import *
@@ -11,56 +10,70 @@ file.close()
 
 
 def create_source(db, event, context):
-    region = os.environ['region']
+    region = os.environ["region"]
+
     # Source Payload
     message_body = event["body-json"]
     api_call_type = "synchronous"
 
     # generate api response
     api_response = dict()
-    api_response['sourcePayload'] = message_body
-    api_response['body'] = dict()
+    api_response["sourcePayload"] = message_body
+    api_response["body"] = dict()
 
-    # generate src_sys_id
-    src_sys_id = generate_src_sys_id(10)
+    # generate a 6 digit source system ID
+    src_sys_id = generate_src_sys_id(6)
 
     # create an entry in the src sys table
-    src_sys_table = global_config['src_sys_table']
-    ingestion_table = global_config['ingestion_table']
+    src_sys_table = global_config["src_sys_table"]
+    ingestion_table = global_config["ingestion_table"]
     print(f"Insert source system info in {src_sys_table} table")
     bucket_name = f"{global_config['fm_prefix']}-{src_sys_id}-{region}"
     try:
-        # Run the CFT to create source system bucket
+        # create folder structure in time and event driven buckets
+        time_drvn_bkt = f"{global_config['fm_prefix']}-time-drvn-inbound-{region}"
+        event_drvn_bkt = f"{global_config['fm_prefix']}-evnt-drvn-inbound-{region}"
+        create_folder_structure(time_drvn_bkt, src_sys_id)
+        create_folder_structure(event_drvn_bkt, src_sys_id)
+
+        # Create source system bucket
+        # create_src_bucket(bucket_name, region)
         run_cft(global_config, src_sys_id, region)
 
         # If source config is present upload the details to the src system table
-        if message_body['src_config']:
-            src_data = message_body['src_config']
-            src_data['src_sys_id'] = src_sys_id
-            src_data['bucket_name'] = bucket_name
+        if message_body["src_config"]:
+            src_data = message_body["src_config"]
+            src_data["src_sys_id"] = src_sys_id
+            src_data["bucket_name"] = bucket_name
             db.insert(table=src_sys_table, data=src_data)
 
-        # If ingestion config is present upload the details to the src sys ingestion attributes
-        if message_body['ingestion_config']:
-            ingestion_data = message_body['ingestion_config']
-            ingestion_data['src_sys_id'] = src_sys_id
-            ingestion_data['bucket_name'] = bucket_name
-            db.insert(table=ingestion_table, data=ingestion_data)
+        # If ingestion config is present
+        # 1. upload the details to the src sys ingestion attributes
+        # 2. Upload the ingestion DB password to DB secrets
+        if message_body["ingestion_config"]:
+            ingestion_data = message_body["ingestion_config"]
+            store_status = store_ingestion_attributes(
+                src_sys_id, bucket_name, ingestion_data, ingestion_table, db, region
+            )
+            api_response["body"]["store_ingestion_attributes"] = store_status
 
         # add the appropriate api response
-        api_response['body']['src_sys_created'] = True
-        api_response['body']['s3_bucket_created'] = True
-        api_response['body']['src_sys_id'] = src_sys_id
-        api_response['body']['src_bucket_name'] = bucket_name
+        api_response["body"]["result"] = "Success"
+        api_response["body"]["src_sys_created"] = True
+        api_response["body"]["s3_bucket_created"] = True
+        api_response["body"]["src_sys_id"] = src_sys_id
+        api_response["body"]["src_bucket_name"] = bucket_name
         response = insert_event_to_dynamoDb(event, context, api_call_type)
-        api_response['sourceCodeDynamoDb'] = response['statusCode']
+        api_response["sourceCodeDynamoDb"] = response["statusCode"]
     except Exception as e:
         print(e)
         rollback_src_sys(db, global_config, src_sys_id, region)
-        api_response['body']['src_sys_created'] = False
-        api_response['body']['s3_structure_created'] = False
-        response = insert_event_to_dynamoDb(event, context, api_call_type, status="Failed")
-        api_response['sourceCodeDynamoDb'] = response['statusCode']
+        api_response["body"]["src_sys_created"] = False
+        api_response["body"]["result"] = "Failed"
+        response = insert_event_to_dynamoDb(
+            event, context, api_call_type, status="Failed"
+        )
+        api_response["sourceCodeDynamoDb"] = response["statusCode"]
     finally:
         return api_response
 
@@ -71,35 +84,35 @@ def read_source(db, event, context):
 
     # API logic here
     src_info = None
-    table = global_config['src_sys_table']
-    fetch_limit = message_body['fetch_limit']
-    src_config = message_body['src_config']
+    table = global_config["src_sys_table"]
+    fetch_limit = message_body["fetch_limit"]
+    src_config = message_body["src_config"]
     # if there is no fetch limit, then get the details of the
     # src_sys_id provided in the src_config
-    if fetch_limit in [None, 'None', '0', 'NONE'] and src_config:
-        src_sys_id = int(src_config['src_sys_id'])
+    if fetch_limit in [None, "None", "0", "NONE"] and src_config:
+        src_sys_id = int(src_config["src_sys_id"])
         condition = ("src_sys_id=%s", [src_sys_id])
-        src_info = db.retrieve_dict(table=table, cols='all', where=condition)
+        src_info = db.retrieve_dict(table=table, cols="all", where=condition)
     # if a fetch limit exists then fetch all the cols of limited src_systems
     elif isinstance(fetch_limit, int) or fetch_limit.isdigit():
         limit = int(fetch_limit)
-        src_info = db.retrieve_dict(table=table, cols='all', limit=limit)
+        src_info = db.retrieve_dict(table=table, cols="all", limit=limit)
     # if neither of the above case satisfies fetch all the info
-    elif fetch_limit == 'all':
-        src_info = db.retrieve_dict(table=table, cols='all')
+    elif fetch_limit == "all":
+        src_info = db.retrieve_dict(table=table, cols="all")
 
     # Generate api response
     api_response = dict()
-    api_response['sourcePayload'] = message_body
-    api_response['body'] = dict()
+    api_response["sourcePayload"] = message_body
+    api_response["body"] = dict()
     if src_info:
-        api_response['body']['exists'] = True
-        api_response['body']['src_info'] = src_info
-        api_response['statusCode'] = 200
+        api_response["body"]["exists"] = True
+        api_response["body"]["src_info"] = src_info
+        api_response["statusCode"] = 200
     else:
-        api_response['body']['exists'] = False
-        api_response['body']['src_info'] = None
-        api_response['statusCode'] = 404
+        api_response["body"]["exists"] = False
+        api_response["body"]["src_info"] = None
+        api_response["statusCode"] = 404
 
     # API event entry in dynamoDb
     response = insert_event_to_dynamoDb(event, context, api_call_type)
@@ -111,24 +124,26 @@ def update_source(db, event, context):
     message_body = event["body-json"]
     api_call_type = "synchronous"
     # parse payload
-    src_config = message_body['src_config']
-    ingestion_config = message_body['ingestion_config']
+    src_config = message_body["src_config"]
+    ingestion_config = message_body["ingestion_config"]
     api_response = dict()
-    api_response['sourcePayload'] = src_config
-    api_response['body'] = dict()
+    api_response["sourcePayload"] = src_config
+    api_response["body"] = dict()
     try:
         src_exists, src_msg = update_source_system(db, src_config, global_config)
-        ing_exists, ing_msg = update_ingestion_attributes(db, ingestion_config, global_config)
-        api_response['body']['src_sys_exists'] = src_exists
-        api_response['body']['src_update_msg'] = src_msg
-        api_response['body']['ingestion_sys_exists'] = ing_exists
-        api_response['body']['ingestion_update_msg'] = ing_msg
+        ing_exists, ing_msg = update_ingestion_attributes(
+            db, ingestion_config, global_config
+        )
+        api_response["body"]["src_sys_exists"] = src_exists
+        api_response["body"]["src_update_msg"] = src_msg
+        api_response["body"]["ingestion_sys_exists"] = ing_exists
+        api_response["body"]["ingestion_update_msg"] = ing_msg
     except Exception as e:
         print(e)
     finally:
         # API event entry in dynamoDb
         response = insert_event_to_dynamoDb(event, context, api_call_type)
-        api_response['sourceCodeDynamoDb'] = response['statusCode']
+        api_response["sourceCodeDynamoDb"] = response["statusCode"]
     return api_response
 
 
@@ -138,35 +153,35 @@ def delete_source(db, event, context):
     """
     message_body = event["body-json"]
     api_call_type = "synchronous"
-    region = os.environ['region']
-    src_config = message_body['src_config']
+    region = os.environ["region"]
+    src_config = message_body["src_config"]
     # API logic here
     api_response = dict()
-    api_response['body'] = dict()
-    api_response['sourcePayload'] = message_body
-    src_sys_id = int(src_config['src_sys_id'])
+    api_response["body"] = dict()
+    api_response["sourcePayload"] = message_body
+    src_sys_id = int(src_config["src_sys_id"])
     if src_sys_present(db, global_config, src_sys_id):
-        api_response['body']['src_sys_present'] = True
+        api_response["body"]["src_sys_present"] = True
         associated = is_associated_with_asset(db, src_sys_id)
-        api_response['body']['asset_association'] = associated
+        api_response["body"]["asset_association"] = associated
         # If it is not associated,source system stack will be deleted
         if not associated:
             try:
                 delete_rds_entry(db, global_config, src_sys_id)
-                api_response['body']['src_sys_deleted'] = True
+                api_response["body"]["src_sys_deleted"] = True
             except Exception as e:
                 print(e)
             # delete the stack
             delete_src_sys_stack(global_config, src_sys_id, region)
         else:
-            api_response['body']['src_sys_deleted'] = False
+            api_response["body"]["src_sys_deleted"] = False
     else:
-        api_response['body']['src_sys_present'] = False
-        api_response['body']['src_sys_deleted'] = False
+        api_response["body"]["src_sys_present"] = False
+        api_response["body"]["src_sys_deleted"] = False
 
     # API event entry in dynamoDb
     response = insert_event_to_dynamoDb(event, context, api_call_type)
-    api_response['sourceCodeDynamoDb'] = response["statusCode"]
+    api_response["sourceCodeDynamoDb"] = response["statusCode"]
     return api_response
 
 
@@ -174,29 +189,33 @@ def lambda_handler(event, context):
     resource = event["context"]["resource-path"][1:]
     taskType = resource.split("/")[0]
     method = resource.split("/")[1]
-    db_secret = os.environ['db_secret']
-    db_region = os.environ['db_region']
+    db_secret = os.environ["db_secret"]
+    db_region = os.environ["db_region"]
     db = Connector(db_secret, db_region, autocommit=True)
 
     print(event)
     print(taskType)
     print(method)
-
-    if event:
-        if method == "health":
-            return {"statusCode": "200", "body": "API Health is good"}
-        elif method == "create":
-            response = create_source(db, event, context)
-            return response
-        elif method == "read":
-            response = read_source(db, event, context)
-            return response
-        elif method == "update":
-            response = update_source(db, event, context)
-            return response
-        elif method == "delete":
-            response = delete_source(db, event, context)
-            return response
-        else:
-            return {"statusCode": "404", "body": "Not found"}
-    db.close()
+    try:
+        if event:
+            if method == "health":
+                return {"statusCode": "200", "body": "API Health is good"}
+            elif method == "create":
+                response = create_source(db, event, context)
+                return response
+            elif method == "read":
+                response = read_source(db, event, context)
+                return response
+            elif method == "update":
+                response = update_source(db, event, context)
+                return response
+            elif method == "delete":
+                response = delete_source(db, event, context)
+                return response
+            else:
+                return {"statusCode": "404", "body": "Not found"}
+    except Exception as e:
+        print(e)
+        db.rollback()
+    finally:
+        db.close()
--- lambda/aws-dl-fmwrk-source-system-api/utils.py
+++ lambda/aws-dl-fmwrk-source-system-api/utils.py
@@ -1,8 +1,10 @@
+import json
 import os
 from random import randrange
 from datetime import datetime
 
 import boto3
+from botocore.exceptions import ClientError
 
 
 def delete_src_sys_stack(global_config, src_sys_id, region):
@@ -15,11 +17,30 @@ def delete_src_sys_stack(global_config, src_sys_id, region):
         print(e)
 
 
+def create_src_bucket(bucket_name, region=None):
+    try:
+        if region is None:
+            s3_client = boto3.client("s3")
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client("s3", region_name=region)
+            location = {"LocationConstraint": region}
+            s3_client.create_bucket(
+                Bucket=bucket_name, CreateBucketConfiguration=location
+            )
+    except ClientError as e:
+        print(e)
+        return False
+    return True
+
+
 def delete_rds_entry(db, global_config, src_sys_id):
     src_sys_id_int = int(src_sys_id)
-    table = global_config['src_sys_table']
-    condition = ('src_sys_id = %s', [src_sys_id_int])
-    db.delete(table, condition)
+    src_table = global_config["src_sys_table"]
+    src_ingstn_table = global_config['ingestion_table']
+    condition = ("src_sys_id = %s", [src_sys_id_int])
+    db.delete(src_table, condition)
+    db.delete(src_ingstn_table, condition)
 
 
 def rollback_src_sys(db, global_config, src_sys_id, region):
@@ -33,12 +54,10 @@ def rollback_src_sys(db, global_config, src_sys_id, region):
 def is_associated_with_asset(db, src_sys_id):
     try:
         # Accessing data asset table
-        table = 'data_asset'
-        condition = ('src_sys_id = %s', [src_sys_id])
+        table = "data_asset"
+        condition = ("src_sys_id = %s", [src_sys_id])
         # Trying to get dynamoDB item with src_sys_id and bucket name as key
-        response = db.retrieve_dict(
-            table, cols='src_sys_id', where=condition
-        )
+        response = db.retrieve_dict(table, cols="src_sys_id", where=condition)
         if response:
             return True
         else:
@@ -49,12 +68,10 @@ def is_associated_with_asset(db, src_sys_id):
 
 def src_sys_present(db, global_config, src_sys_id):
     try:
-        table = global_config['src_sys_table']
-        condition = ('src_sys_id = %s', [src_sys_id])
+        table = global_config["src_sys_table"]
+        condition = ("src_sys_id = %s", [src_sys_id])
         # Trying to get dynamoDB item with src_sys_id and bucket name as key
-        response = db.retrieve_dict(
-            table, cols='src_sys_id', where=condition
-        )
+        response = db.retrieve_dict(table, cols="src_sys_id", where=condition)
         # If item with the specified src_sys_id is present,the response contains "Item" in it
         if response:
             # Returns True if src_sys_id is present
@@ -67,7 +84,7 @@ def src_sys_present(db, global_config, src_sys_id):
 
 
 def generate_src_sys_id(n):
-    return int(f'{randrange(1, 10 ** n):03}')
+    return int(f"{randrange(1, 10 ** n):03}")
 
 
 def run_cft(global_config, src_sys_id, region):
@@ -105,7 +122,7 @@ def run_cft(global_config, src_sys_id, region):
 def insert_event_to_dynamoDb(
     event, context, api_call_type, status="success", op_type="insert"
 ):
-    cur_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    cur_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     aws_request_id = context.aws_request_id
     log_group_name = context.log_group_name
     log_stream_name = context.log_stream_name
@@ -164,46 +181,111 @@ def insert_event_to_dynamoDb(
     }
 
 
+def create_folder_structure(bucket, src_id):
+    client = boto3.client("s3")
+    dummy_file = "dummy.txt"
+    KeyFileName = f"{src_id}/{dummy_file}"
+    body = b"Creating the folder structure"
+    client.put_object(Body=body, Bucket=bucket, Key=KeyFileName)
+
+
+def secret_exists(secret_id, region):
+    client = boto3.client(service_name="secretsmanager", region_name=region)
+    secret_list = client.list_secrets()["SecretList"]
+    secrets = [i["Name"] for i in secret_list]
+    if secret_id in secrets:
+        return True
+    else:
+        return False
+
+
+def store_secret(db_secret, src_sys_id, region):
+    secret_id = f"dl-fmwrk-ingstn-db-secrets-{src_sys_id}"
+    if secret_exists(secret_id, region):
+        store_status = "Credential already exists"
+    else:
+        try:
+            client = boto3.client(service_name="secretsmanager", region_name=region)
+            if db_secret:
+                secret = {f"{src_sys_id}": f"{db_secret}"}
+                secret_string = json.dumps(secret)
+                client.create_secret(
+                    Name=secret_id,
+                    Description="Credentials of Ingestion DB",
+                    SecretString=secret_string,
+                    Tags=[
+                        {"Key": "src_sys_id", "Value": f"{src_sys_id}"},
+                    ],
+                )
+                store_status = f"Stored the credentials for src sys: {src_sys_id}"
+            else:
+                store_status = f"Missing credentials for src sys"
+        except Exception as e:
+            store_status = e
+    return store_status
+
+
+def store_ingestion_attributes(
+    src_sys_id, bucket_name, ingestion_data, ingestion_table, db, region
+):
+    store_status = ""
+    ingestion_data["src_sys_id"] = src_sys_id
+    ingestion_data["ingstn_src_bckt_nm"] = bucket_name
+    if "db_pass" in ingestion_data:
+        db_secret = ingestion_data["db_pass"]
+        ingestion_data.pop("db_pass", None)
+    else:
+        db_secret = None
+    try:
+        db.insert(table=ingestion_table, data=ingestion_data)
+        store_status = "Stored the ingestion Attributes and "
+        store_status += store_secret(db_secret, src_sys_id, region)
+    except Exception as e:
+        store_status = e
+    return store_status
+
+
 def update_source_system(db, src_config: dict, global_config: dict):
-    exists, message = None, 'unable to update'
-    non_editable_params = ['src_sys_id', 'bucket_name']
+    exists, message = None, "unable to update"
+    non_editable_params = ["src_sys_id", "bucket_name"]
     if src_config:
-        src_sys_id = src_config['src_sys_id']
-        data_to_update = src_config['update_data']
-        src_sys_table = global_config['src_sys_table']
+        src_sys_id = src_config["src_sys_id"]
+        data_to_update = src_config["update_data"]
+        src_sys_table = global_config["src_sys_table"]
         condition = ("src_sys_id=%s", [src_sys_id])
-        exists = True if db.retrieve(src_sys_table, 'src_sys_id', condition) else False
+        exists = True if db.retrieve(src_sys_table, "src_sys_id", condition) else False
         if exists:
             if not any(x in data_to_update.keys() for x in non_editable_params):
                 db.update(src_sys_table, data_to_update, condition)
-                message = 'updated'
+                message = "updated"
             else:
                 message = "Trying to update a non editable parameter: src_sys_id / bucket name"
         else:
             message = "Source system DNE"
     else:
-        message = 'No update config provided'
+        message = "No update config provided"
     return exists, message
 
 
 def update_ingestion_attributes(db, ingestion_config: dict, global_config: dict):
-    exists, message = None, 'unable to update'
-    non_editable_params = ['src_sys_id', 'ingstn_src_bckt_nm']
+    exists, message = None, "unable to update"
+    non_editable_params = ["src_sys_id", "ingstn_src_bckt_nm"]
     if ingestion_config:
-        src_sys_id = ingestion_config['src_sys_id']
-        data_to_update = ingestion_config['update_data']
-        ingestion_table = global_config['ingestion_table']
+        src_sys_id = ingestion_config["src_sys_id"]
+        data_to_update = ingestion_config["update_data"]
+        ingestion_table = global_config["ingestion_table"]
         condition = ("src_sys_id=%s", [src_sys_id])
-        exists = True if db.retrieve(ingestion_table, 'src_sys_id', condition) else False
+        exists = (
+            True if db.retrieve(ingestion_table, "src_sys_id", condition) else False
+        )
         if exists:
             if not any(x in data_to_update.keys() for x in non_editable_params):
                 db.update(ingestion_table, data_to_update, condition)
-                message = 'updated'
+                message = "updated"
             else:
                 message = "Trying to update a non editable parameter: src_sys_id / bucket name"
         else:
             message = "Source System DNE"
     else:
-        message = 'No update config provided'
+        message = "No update config provided"
     return exists, message
-
