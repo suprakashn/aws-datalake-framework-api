48bffc7c3d40a9c594cb5705b4c90f94735f07eb
Aadil Shaik <aadil.shaik@tigeranalytics.local>
2022-06-21 17:19:02 +0530

    added data asset catalogs api

---
+++ lambda/aws-dl-fmwrk-data-asset-catalogs-api/utils.py
@@ -0,0 +1,70 @@
+from connector import Connector
+import boto3
+import os
+from datetime import datetime
+import json
+
+
+def getGlobalParams():
+    with open('globalConfig.json', "r") as json_file:
+        json_config = json.load(json_file)
+        return json_config
+
+
+def get_database():
+    db_secret = os.environ['secret_name']
+    db_region = os.environ['secret_region']
+    conn = Connector(secret=db_secret, region=db_region, autocommit=False)
+    return conn
+
+
+def insert_event_to_dynamoDb(event, context, api_call_type, status="success", op_type="insert"):
+    cur_time = datetime.now()
+    aws_request_id = context.aws_request_id
+    log_group_name = context.log_group_name
+    log_stream_name = context.log_stream_name
+    function_name = context.function_name
+    method_name = event["context"]["resource-path"]
+    query_string = event["params"]["querystring"]
+    payload = event["body-json"]
+
+    client = boto3.resource("dynamodb")
+    table = client.Table("aws-dl-fmwrk-api-events")
+
+    if op_type == "insert":
+        response = table.put_item(
+            Item={
+                "aws_request_id": aws_request_id,
+                "method_name": method_name,
+                "log_group_name": log_group_name,
+                "log_stream_name": log_stream_name,
+                "function_name": function_name,
+                "query_string": query_string,
+                "payload": payload,
+                "api_call_type": api_call_type,
+                "modified ts": str(cur_time),
+                "status": status,
+            })
+    else:
+        response = table.update_item(
+            Key={
+                'aws_request_id': aws_request_id,
+                'method_name': method_name,
+            },
+            ConditionExpression="attribute_exists(aws_request_id)",
+            UpdateExpression='SET status = :val1',
+            ExpressionAttributeValues={
+                ':val1': status,
+            }
+        )
+
+    if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+        body = "Insert/Update of the event with aws_request_id=" + \
+            aws_request_id + " completed successfully"
+    else:
+        body = "Insert/Update of the event with aws_request_id=" + aws_request_id + " failed"
+
+    return {
+        "statusCode": response["ResponseMetadata"]["HTTPStatusCode"],
+        "body": body,
+    }
