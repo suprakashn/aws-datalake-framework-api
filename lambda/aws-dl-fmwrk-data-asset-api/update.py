b0bbca54bb963e739e7e2a17dca701b0ec2acc81
Aadil Shaik <aadil.shaik@tigeranalytics.local>
2022-07-14 14:42:43 +0530

    refactored update.py in data-asset-api

--- lambda/aws-dl-fmwrk-data-asset-api/update.py
+++ lambda/aws-dl-fmwrk-data-asset-api/update.py
@@ -4,9 +4,74 @@ from utils import *
 from api_response import Response
 
 
+def update_asset_info(message_body, asset_id, src_sys_id, message_keys, database):
+    if "asset_info" in message_keys:
+        # asset_nm = message_body["asset_info"]["asset_nm"]
+        # if "rs_load_ind" in message_body["asset_info"].keys():
+        #    rs_load_ind = message_body["asset_info"]["rs_load_ind"]
+        #    if rs_load_ind == True:
+        #        message_body["asset_info"]["rs_stg_table_nm"] = f"{asset_nm}_stg"
+        #    else:
+        #        message_body["asset_info"]["rs_stg_table_nm"] = None
+        asset_info = message_body["asset_info"].copy()
+        asset_info["modified_ts"] = "now()"
+        where_clause = ("asset_id=%s and src_sys_id=%s", [
+            asset_id, src_sys_id])
+        database.update(
+            table="data_asset",
+            data=asset_info,
+            where=where_clause
+        )
+        return asset_info
+    return {}
+
+
+def update_asset_attributes(message_body, asset_id, message_keys, database):
+    if "asset_attributes" in message_keys:
+        asset_attributes = message_body["asset_attributes"].copy()
+        for data in asset_attributes:
+            col_id = data["col_id"]
+            data["modified_ts"] = "now()"
+            where_clause = (
+                "asset_id=%s and col_id=%s", [asset_id, col_id])
+            database.update(
+                table="data_asset_attributes",
+                data=data,
+                where=where_clause
+            )
+        return asset_attributes
+    return []
+
+
+def update_ingestion_attributes(message_body, asset_id, src_sys_id, message_keys, database):
+    if "ingestion_attributes" in message_keys:
+        ingestion_attributes = message_body["ingestion_attributes"].copy()
+        where_clause = ("asset_id=%s and src_sys_id=%s", [
+            asset_id, src_sys_id])
+        database.update(
+            table="data_asset_ingstn_atrbts",
+            data=ingestion_attributes,
+            where=where_clause
+        )
+        if "frequency" in message_body["ingestion_attributes"].keys():
+            # Deleting previous dag
+            client = boto3.client("s3")
+            airflow_bucket = 'dl-fmwrk-mwaa-us-east-2'
+            file_name = f"dags/{src_sys_id}_{asset_id}_worflow.py"
+            client.delete_object(Bucket=airflow_bucket, Key=file_name)
+            # Creating new dag
+            freq = message_body["ingestion_attributes"]["frequency"]
+            glue_airflow_trigger(
+                source_id=str(src_sys_id),
+                asset_id=str(asset_id),
+                schedule=freq
+            )
+        return ingestion_attributes
+    return {}
+
+
 def update_asset(event, method, database):
     message_body = event["body-json"]
-    payload = message_body.copy()
 
     # API logic here
     # -----------
@@ -16,79 +81,54 @@ def update_asset(event, method, database):
     message_keys = message_body.keys()
 
     try:
-        if "asset_info" in message_keys:
-            # asset_nm = message_body["asset_info"]["asset_nm"]
-            # if "rs_load_ind" in message_body["asset_info"].keys():
-            #    rs_load_ind = message_body["asset_info"]["rs_load_ind"]
-            #    if rs_load_ind == True:
-            #        message_body["asset_info"]["rs_stg_table_nm"] = f"{asset_nm}_stg"
-            #    else:
-            #        message_body["asset_info"]["rs_stg_table_nm"] = None
-            data_dataAsset = message_body["asset_info"]
-            data_dataAsset["modified_ts"] = "now()"
-            dataAsset_where = ("asset_id=%s", [asset_id])
-            database.update(
-                table="data_asset",
-                data=data_dataAsset,
-                where=dataAsset_where
-            )
-        if "asset_attributes" in message_keys:
-            data_dataAssetAttributes = message_body["asset_attributes"]
-            for data in data_dataAssetAttributes:
-                col_id = data["col_id"]
-                data["modified_ts"] = "now()"
-                dataAssetAttributes_where = (
-                    "asset_id=%s and col_id=%s", [asset_id, col_id])
-                database.update(
-                    table="data_asset_attributes",
-                    data=data,
-                    where=dataAssetAttributes_where
-                )
-        if "ingestion_attributes" in message_keys:
-            data_ingestion = message_body["ingestion_attributes"]
-            ingestion_where = ("asset_id=%s and src_sys_id=%s", [
-                               asset_id, src_sys_id])
-            database.update(
-                table="data_asset_ingstn_atrbts",
-                data=data_ingestion,
-                where=ingestion_where
-            )
-            if "frequency" in message_body["ingestion_attributes"].keys():
-                # Deleting previous dag
-                client = boto3.client("s3")
-                airflow_bucket = 'dl-fmwrk-mwaa-us-east-2'
-                file_name = f"dags/{src_sys_id}_{asset_id}_worflow.py"
-                client.delete_object(Bucket=airflow_bucket, Key=file_name)
-                # Creating new dag
-                freq = message_body["ingestion_attributes"]["frequency"]
-                glue_airflow_trigger(
-                    source_id=src_sys_id,
-                    asset_id=asset_id,
-                    schedule=freq
-                )
+        asset_info = update_asset_info(
+            message_body,
+            asset_id,
+            src_sys_id,
+            message_keys,
+            database
+        )
+        asset_attributes = update_asset_attributes(
+            message_body,
+            asset_id,
+            message_keys,
+            database
+        )
+        ingestion_attributes = update_ingestion_attributes(
+            message_body,
+            asset_id,
+            src_sys_id,
+            message_keys,
+            database
+        )
+
+        data_adv_dq = []
         if "adv_dq_rules" in message_keys:
             update_adv_dq(
                 database, message_body, src_sys_id, asset_id
             )
+            data_adv_dq = message_body["adv_dq_rules"]
         database.close()
         status = True
-        status_code = 203
-
+        body = {
+            "asset_info": asset_info,
+            "asset_attributes": asset_attributes,
+            "ingestion_attributes": ingestion_attributes,
+            "adv_dq": data_adv_dq
+        }
     except Exception as e:
         print(e)
         database.rollback()
         database.close()
         status = False
-        status_code = 403
-        payload = event["body-json"]
-        message_body = {}
+        body = {}
 
     # -----------
 
     response = Response(
         method=method,
         status=status,
-        body=message_body,
-        payload=payload
+        body=body,
+        payload=message_body
     )
     return response.get_response()
