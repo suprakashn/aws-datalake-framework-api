be7e0892a2482084edfb16ee7123d4711b961a13
Aadil Shaik <aadil.shaik@tigeranalytics.local>
2022-07-14 10:58:05 +0530

    refactored data-asset-info-api response structure

--- lambda/aws-dl-fmwrk-data-asset-info-api/lambda_function.py
+++ lambda/aws-dl-fmwrk-data-asset-info-api/lambda_function.py
@@ -3,61 +3,74 @@ from utils import *
 from api_response import Response
 
 
-def read_asset(event, context, database):
+def read_asset(event, method, database):
     message_body = event["body-json"]
-    api_call_type = "synchronous"
 
     # API logic here
     # -----------
+    try:
+        asset_columns = [
+            "asset_id",
+            "src_sys_id",
+            "target_id",
+            "file_header",
+            "multipartition",
+            "file_type",
+            "asset_nm",
+            "source_path",
+            "target_path",
+            "trigger_file_pattern",
+            "file_delim",
+            "file_encryption_ind",
+            "athena_table_name",
+            "asset_owner",
+            "support_cntct",
+            "rs_load_ind",
+            "rs_stg_table_nm"
+        ]
 
-    asset_columns = [
-        "asset_id",
-        "src_sys_id",
-        "target_id",
-        "file_header",
-        "multipartition",
-        "file_type",
-        "asset_nm",
-        "source_path",
-        "target_path",
-        "trigger_file_pattern",
-        "file_delim",
-        "file_encryption_ind",
-        "athena_table_name",
-        "asset_owner",
-        "support_cntct",
-        "rs_load_ind",
-        "rs_stg_table_nm"
-    ]
-
-    # Getting the asset id and source system id
-    src_sys_id = message_body["src_sys_id"]
-    where_clause = ("src_sys_id=%s", [src_sys_id])
-    list_asset = database.retrieve_dict(
-        table="data_asset",
-        cols=asset_columns,
-        where=where_clause
-    )
-    database.close()
-    if list_asset:
-        status = True
-        status_code = 202
-        body = list_asset
-    else:
+        # Getting the asset id and source system id
+        src_sys_id = message_body["src_sys_id"]
+        where_clause = ("src_sys_id=%s", [src_sys_id])
+        list_asset = database.retrieve_dict(
+            table="data_asset",
+            cols=asset_columns,
+            where=where_clause
+        )
+        database.close()
+        if list_asset:
+            status = True
+            body = list_asset
+        else:
+            status = False
+            body = {}
+    except Exception as e:
+        body = str(e)
         status = False
-        status_code = 402
-        body = {}
+        database.close()
 
     # -----------
+    response = Response(
+        method=method,
+        status=status,
+        body=body,
+        payload=message_body
+    )
+    return response.get_response()
+
+
+def read(event, context, method):
+    database = get_database()
+    api_response = read_asset(event, method, database)
     # API event entry in dynamoDb
-    response = insert_event_to_dynamoDb(event, context, api_call_type)
-    return{
-        "statusCode": status_code,
-        "status": status,
-        "sourceCodeDynamoDb": response["statusCode"],
-        "body": body,
-        "payload": message_body
-    }
+    api_call_type = "synchronous"
+    if api_response['responseStatus']:
+        insert_event_to_dynamoDb(event, context, api_call_type)
+    else:
+        insert_event_to_dynamoDb(
+            event, context, api_call_type, status="Failed"
+        )
+    return api_response
 
 
 def lambda_handler(event, context):
@@ -71,19 +84,10 @@ def lambda_handler(event, context):
 
     if event:
         if method == "health":
-            response = Response(method, True, "body", None)
-            return response.get_response()
+            return {"statusCode": "200", "body": "API Health is good"}
 
         elif method == "read":
-            db = get_database()
-            generated_response = read_asset(event, context, database=db)
-            response = Response(
-                method=method,
-                status=generated_response["status"],
-                body=generated_response["body"],
-                payload=generated_response["payload"]
-            )
-            return response.get_response()
-
+            response = read(event, context, method)
+            return response
         else:
             return {"statusCode": "404", "body": "Not found"}
